8d374010b598cb12ee440a5e7a672f83844ca9d9
Javier Briseno <jbf31293@gmail.com>
2023-03-11 15:54:47 -0600

    feat: finish work

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let fields = document.querySelectorAll("#person-form input");
+let personObject = {};
+
+fields.forEach((field) => {
+  field.addEventListener("keyup", (event) => {
+    let property = event.target.name;
+    let value = event.target.value;
+    personObject[property] = value;
+    console.log(personObject); 
+  });
+});
+
+const tableBody = document.getElementById("people-list")
+
+let element = document.querySelector("#my-button")
+element.addEventListener("click", () => {
+    let tableRow = document.createElement("tr")
+    tableBody.appendChild(tableRow)
+    let deleteBtn = document.createElement("button")
+    deleteBtn.textContent = "Eliminar"
+    for (let prop in personObject) {
+        injectTextNodeToTableRow(tableRow, personObject[prop], deleteBtn)
+    }
+})
+
+function injectTextNodeToTableRow(tableRow, text, deleteBtn) {
+    let cell = document.createElement("td")
+    let textNode = document.createTextNode(`${text}`)
+    cell.appendChild(textNode)
+    tableRow.appendChild(cell)
+    tableRow.appendChild(deleteBtn)
+
+    deleteBtn.addEventListener("click", (event) => {
+        event.target.parentNode.remove()
+    })
+}
+
+
+
+
